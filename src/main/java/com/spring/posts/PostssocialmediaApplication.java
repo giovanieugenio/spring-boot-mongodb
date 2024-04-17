d3bd7f6bb491e3f4d73f309741d1b0d9a24e0125
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-04-17 15:51:42 -0300

    first commmit: created project

---
+++ src/main/java/com/spring/posts/PostssocialmediaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostssocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostssocialmediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/posts/PostssocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostssocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
