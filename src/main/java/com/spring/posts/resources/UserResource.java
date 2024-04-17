673a4125fcfbb1ee1c550e35a5daf748bf44abad
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-04-17 20:16:13 -0300

    insertion user post

--- src/main/java/com/spring/posts/resources/UserResource.java
+++ src/main/java/com/spring/posts/resources/UserResource.java
@@ -1,13 +1,19 @@
 package com.spring.posts.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.spring.posts.domain.User;
 import com.spring.posts.dto.UserDTO;
@@ -25,5 +31,25 @@ public class UserResource {
 		List<User> list = userService.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+	    Optional<User> user = userService.findById(id);
+	    if (user != null) {
+	    	User obj = user.get();
+	        UserDTO userDTO = new UserDTO(obj);
+	        return ResponseEntity.ok().body(userDTO);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = userService.fromDTO(objDto);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/spring/posts/services/UserService.java
+++ src/main/java/com/spring/posts/services/UserService.java
@@ -1,12 +1,15 @@
 package com.spring.posts.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.posts.domain.User;
+import com.spring.posts.dto.UserDTO;
 import com.spring.posts.repositories.UserRepository;
+import com.spring.posts.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,21 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public Optional<User> findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Object not found.");
+		}
+		return user;
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
